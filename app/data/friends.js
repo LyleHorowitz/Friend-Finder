e400163ec159df1a22bafb20dd23bad8a9ce82f9
Lyle <5846films@gmail.com>
2017-08-24 22:11:34 -0700

    added routing and server

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,98 +1,101 @@
-[
+var friends = [
 {
-"name": "Frankenstein",
-"photo": "https://interestingliterature.files.wordpress.com/2013/06/frankenstein.gif",
-"scores": [
-"5",
-"1",
-"4",
-"4",
-"5",
-"1",
-"2",
-"5",
-"4",
-"1"
+name: "Frankenstein",
+photo: "https://interestingliterature.files.wordpress.com/2013/06/frankenstein.gif",
+scores: [
+5,
+1,
+4,
+4,
+5,
+1,
+2,
+5,
+4,
+1
 ]
 },
 {
-"name": "Dracula",
-"photo": "https://media.giphy.com/media/PRy5GPH1uAeNa/giphy.gif",
-"scores": [
-"4",
-"2",
-"5",
-"1",
-"3",
-"2",
-"2",
-"1",
-"3",
-"2"
+name: "Dracula",
+photo: "https://media.giphy.com/media/PRy5GPH1uAeNa/giphy.gif",
+scores: [
+4,
+2,
+5,
+1,
+3,
+2,
+2,
+1,
+3,
+2
 ]
 },
 {
-"name": "The Invisible Man",
-"photo": "http://www.geeksofdoom.com/GoD/img/2016/02/the-invisible-man.jpg",
-"scores": [
-"5",
-"2",
-"2",
-"2",
-"4",
-"1",
-"3",
-"2",
-"5",
-"5"
+name: "The Invisible Man",
+photo: "http://www.geeksofdoom.com/GoD/img/2016/02/the-invisible-man.jpg",
+scores: [
+5,
+2,
+2,
+2,
+4,
+1,
+3,
+2,
+5,
+5
 ]
 },
 {
-"name": "The Wolf-Man",
-"photo": "http://nerdist.com/wp-content/uploads/2017/05/thewolfman.jpg",
-"scores": [
-"3",
-"3",
-"4",
-"2",
-"2",
-"1",
-"3",
-"2",
-"2",
-"3"
+name: "The Wolf-Man",
+photo: "http://nerdist.com/wp-content/uploads/2017/05/thewolfman.jpg",
+scores: [
+3,
+3,
+4,
+2,
+2,
+1,
+3,
+2,
+2,
+3
 ]
 },
 {
-"name": "The Bride of Frankenstein",
-"photo": "http://media.popculture.com/2017/05/bride-of-frankenstein-movie-universal-dark-universe-2019-20001851.jpg",
-"scores": [
-"4",
-"3",
-"4",
-"1",
-"5",
-"2",
-"5",
-"3",
-"1",
-"4"
+name: "The Bride of Frankenstein",
+photo: "http://media.popculture.com/2017/05/bride-of-frankenstein-movie-universal-dark-universe-2019-20001851.jpg",
+scores: [
+4,
+3,
+4,
+1,
+5,
+2,
+5,
+3,
+1,
+4
 ]
 },
 {
-"name": "The Creature From The Black Lagoon",
-"photo": "http://cdn1-www.comingsoon.net/assets/uploads/2016/03/file_748742_Creature4.jpg",
-"scores": [
-"4",
-"4",
-"2",
-"3",
-"2",
-"2",
-"3",
-"2",
-"4",
-"5"
+name: "The Creature From The Black Lagoon",
+photo: "http://cdn1-www.comingsoon.net/assets/uploads/2016/03/file_748742_Creature4.jpg",
+scores: [
+4,
+4,
+2,
+3,
+2,
+2,
+3,
+2,
+4,
+5
 ]
 }
-]
+];
+
+module.exports = friends;
+
